c2dc462efbbfdc36ea2f4c12b01b4e38cc4d6c8f
BBDbhagyashrithakur <Bhagyashri.Thakur@bbd.co.za>
2024-02-18 17:17:50 +0530

    add view, procedure , function and scalar function

---
+++ database/migrations/V009_20240218114425__function.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION GetStudentsByName(@first_name VARCHAR(255))
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT s.student_id, s.first_name, s.last_name, s.email
+    FROM student s
+    WHERE s.first_name = @first_name
+);
---
+++ database/migrations/V010_20240218114527__scalarfunction.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION GetStudentsByCount()
+RETURNS TABLE
+AS
+RETURN
+( 
+SELECT count(student_id) AS NumStudents FROM Student
+);
+
+
+select * from GetStudentsByCount();
