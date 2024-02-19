afa6a951596a0c7bdfd8df58d51fd25bf4b79aae
BBDbhagyashrithakur <Bhagyashri.Thakur@bbd.co.za>
2024-02-19 23:38:36 +0530

    create tables

--- internship/migrations/V003_20240219164346__altertables.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE tpo add FOREIGN KEY(college_id) REFERENCES college(college_id)
-
-ALTER TABLE student add FOREIGN KEY(college_id) REFERENCES college(college_id)
-
-ALTER TABLE job_post add FOREIGN KEY(company_id) REFERENCES company(company_id)
-
-ALTER TABLE application add FOREIGN KEY(job_id) REFERENCES job_post(job_id)
-
-ALTER TABLE application add FOREIGN KEY(student_id) REFERENCES student(student_id)
--- internship/migrations/V005_20240219175052__BhagyashriT.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO college (college_name, contact)
-VALUES
-('Indian Institute of Technology','1234556780'),
-('University of Delhi', '1289876543'),
-('Indian Institute of Management Ahmedabad', '1287654321'),
-('Anna University Chennai', '1988776655'),
-('Jawaharlal Nehru University ', '1998887770');
