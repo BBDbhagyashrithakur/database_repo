d87b4a03cba520a27580b27f8534dde40f385b71
BBDbhagyashrithakur <Bhagyashri.Thakur@bbd.co.za>
2024-02-19 23:29:04 +0530

    change insert data into tpo  table

--- internship/migrations/V006_20240219175347__tpotable.sql
+++ internship/migrations/V006_20240219175347__tpotable.sql
@@ -1,7 +1,7 @@
 INSERT INTO tpo (first_name, last_name, contact, email, department, city_name, postal_code, college_id)
 VALUES
-('John', 'Doe', '1234567890', 'john.doe@example.com', 'Engineering', 'New York', '10001', 1),
-('Alice', 'Smith', '1876543210', 'alice.smith@example.com', 'Computer Science', 'San Francisco', '94102', 2),
-('Mike', 'Johnson', '1555555555', 'mike.johnson@example.com', 'Business Administration', 'Chicago', '60601', 3),
-('Emily', 'Brown', '1998887770', 'emily.brown@example.com', 'Marketing', 'Los Angeles', '90001', 4),
-('Sarah', 'Wilson', '1112223330', 'sarah.wilson@example.com', 'Finance', 'Houston', '77002', 5);
+('John', 'Doe', '1234567890', 'john.doe@example.com', 'Engineering', 'New York', '10001', 6),
+('Alice', 'Smith', '1876543210', 'alice.smith@example.com', 'Computer Science', 'San Francisco', '94102', 7),
+('Mike', 'Johnson', '1555555555', 'mike.johnson@example.com', 'Business Administration', 'Chicago', '60601', 8),
+('Emily', 'Brown', '1998887770', 'emily.brown@example.com', 'Marketing', 'Los Angeles', '90001', 9),
+('Sarah', 'Wilson', '1112223330', 'sarah.wilson@example.com', 'Finance', 'Houston', '77002', 10);
