24f410d5e47c0a0a592b2da6c4c0cc46262a7271
BBDbhagyashrithakur <Bhagyashri.Thakur@bbd.co.za>
2024-02-20 00:00:03 +0530

    insert data into student tables

--- internship/migrations/V006_20240219182932__studentdata.sql
+++ internship/migrations/V006_20240219182932__studentdata.sql
@@ -2,7 +2,7 @@ INSERT INTO student (first_name, last_name, contact, email, department, resume, 
 VALUES
 ('Emma', 'Johnson', '1245678990', 'emma.johnson@example.com', 'Computer Science', 'emma-resume.pdf', 1),
 ('Michael', 'Williams', '1876543210', 'michael.williams@example.com', 'Engineering', 'Michael-resume.pdf', 2),
-('Sophia', 'Brown', '1555555555', 'sophia.brown@example.com', 'Sophia-resume.pdf', 3),
+('Sophia', 'Brown', '1555555555', 'sophia.brown@example.com', 'Engineering','Sophia-resume.pdf', 3),
 ('Oliver', 'Davis', '1998887770', 'oliver.davis@example.com', 'Marketing', 'Oliver-resume.pdf', 4),
 ('Ava', 'Martinez', '1112223330', 'ava.martinez@example.com', 'Finance', 'Ava-resume.pdf', 5);
 
