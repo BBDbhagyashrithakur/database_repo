8a61f844c03c027520827fd529416a8ba52489a3
BBDbhagyashrithakur <Bhagyashri.Thakur@bbd.co.za>
2024-02-20 00:22:21 +0530

    insert data into  tables

--- internship/migrations/V008_20240219183502__companydata.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO company (company_name, contact, email, location, industry_type)
-VALUES
-('ABC Technologies', '1234567890', 'info@abc.com', 'New York', 'Technology'),
-('XYZ Corporation', '9876543210', 'contact@xyzcorp.com', 'San Francisco', 'Finance'),
-('PQR Solutions', '5555555555', 'info@pqrsolutions.com', 'Chicago', 'Consulting'),
-('LMN Enterprises', '9998887770', 'contact@lmnent.com', 'Los Angeles', 'Retail'),
-('RST Industries', '1112223330', 'info@rstindustries.com', 'Houston', 'Manufacturing');
--- internship/migrations/V009_20240219183744__jobpost.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO job_post (job_title, job_role, job_desc, company_id)
-VALUES
-('Software Engineer', 'Development', 'Design and implement software solutions', 1),
-('Marketing Manager', 'Marketing', 'Develop and implement marketing strategies', 2),
-('Sales Representative', 'Sales', 'Generate sales leads and close deals', 3),
-('Financial Analyst', 'Finance', 'Analyze financial data and provide insights', 4),
-('HR Coordinator', 'Human Resources', 'Manage HR processes and employee relations', 5);
-
