b8e0df0c92886f6ab5fe90eedbc7070722d64f53
ritu22kar <159768617+ritu22kar@users.noreply.github.com>
2024-02-23 11:40:08 +0530

    Update V003_20240219180905__altertable.sql
    new tables alter

--- internship/migrations/V003_20240219180905__altertable.sql
+++ internship/migrations/V003_20240219180905__altertable.sql
@@ -1,9 +1,23 @@
-ALTER TABLE tpo add FOREIGN KEY(college_id) REFERENCES college(college_id)
+ALTER TABLE State add FOREIGN KEY(CountryId) REFERENCES Country(CountryId);
+ALTER TABLE City add FOREIGN KEY(StateId) REFERENCES State(StateId);
+ALTER TABLE Address add FOREIGN KEY(CityId) REFERENCES City(CityId);
 
-ALTER TABLE student add FOREIGN KEY(college_id) REFERENCES college(college_id)
+ALTER TABLE Student add FOREIGN KEY(UserId) REFERENCES User(Id)
+ALTER TABLE Student add FOREIGN KEY(CollegeId) REFERENCES College(CollegeId)
+ALTER TABLE Student add FOREIGN KEY(DepartmentId) REFERENCES Department(DepartmentId)
 
-ALTER TABLE job_post add FOREIGN KEY(company_id) REFERENCES company(company_id)
+ALTER TABLE PlacementOfficer add FOREIGN KEY(UserId) REFERENCES User(Id)
+ALTER TABLE PlacementOfficer add FOREIGN KEY(CollegeId) REFERENCES College(CollegeId)
+ALTER TABLE PlacementOfficer add FOREIGN KEY(JobId) REFERENCES Jobpost(JobId)
 
-ALTER TABLE application add FOREIGN KEY(job_id) REFERENCES job_post(job_id)
+ ALTER TABLE college add FOREIGN KEY(InstituteId) REFERENCES instituteType(InstituteId)
+ALTER TABLE college add FOREIGN KEY(AddressId) REFERENCES address(AddressId)
 
-ALTER TABLE application add FOREIGN KEY(student_id) REFERENCES student(student_id)
+ALTER TABLE Company add FOREIGN KEY(AddressId) REFERENCES Address(AddressId)
+ALTER TABLE Company add FOREIGN KEY(CompanytypeId) REFERENCES Companytype(CompanytypeId)
+
+ALTER TABLE Jobpost add FOREIGN KEY(CompanyId) REFERENCES Company(CompanyId)
+ALTER TABLE Jobpost add FOREIGN KEY(SkillId) REFERENCES Skills(SkillId)
+
+ALTER TABLE Application add FOREIGN KEY(StudentId) REFERENCES Student(StudentId)
+ALTER TABLE Application add FOREIGN KEY(PlacementOffId) REFERENCES PlacementOfficer(PlacementOffId)
